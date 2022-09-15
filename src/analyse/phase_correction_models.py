Fixed bug in average_bpms, finished individual phase correction plots, started work on tempo slope animations

src/analyse/phase_correction_models.py
@@ -1,9 +1,8 @@
 import numpy as np
 import pandas as pd
 import statsmodels.formula.api as smf
-from pymer4.models import Lmer
 from prepare_data import zip_same_conditions_together, generate_df, append_zoom_array, reg_func, average_bpms
-from src.visualise.phase_correction_graphs import make_pairgrid, create_prediction_plots, make_polar
+from src.visualise.phase_correction_graphs import make_pairgrid, make_single_condition_phase_correction_plot, make_polar
 
 
 def delay_heard_onsets_by_latency(df: pd.DataFrame = object, ) -> pd.DataFrame:
@@ -96,54 +95,10 @@ def predict_from_model(md, df: pd.DataFrame) -> pd.DataFrame:
     pred = pd.Series(np.insert(np.cumsum(md.predict()) + df['live_prev_onset'].loc[0], 0, df['live_prev_onset'].loc[0]))
     df['predicted_onset'] = pred
     df['predicted_ioi'] = pred.diff()
-    return df[['live_prev_onset', 'predicted_onset', 'live_prev_ioi', 'predicted_ioi']]
-
-
-def phase_correction_lmer(df: pd.DataFrame,
-                          mod: str = 'correction_partner~latency*jitter+instrument'):
-    """
-    For every trial, creates a linear mixed effects model in Pymer4. Default model:
-    DV: phase correction to partner (continuous)
-    Fixed IVs: latency, jitter, instrument (categorical)
-    Random IVs: Random slope + intercept for block
-    """
-    for idx, grp in df.groupby('trial'):
-        md = Lmer(mod, data=grp).fit(factors={
-            "latency": ["0", "23", "45", "90", "180"],
-            "jitter": ["0.0", "0.5", "1.0", ],
-            "instrument": ['Keys', 'Drums']
-        })
-        print(md)
-
-
-def compare_linear_mixed_model(df: pd.DataFrame,
-                               linear_mod: str = 'correction_partner~C(latency)+C(jitter)+C(instrument)+C(block)',
-                               mixed_mod: str = 'correction_partner~latency+jitter+instrument+(1+latency+jitter+instrument|block)') -> pd.DataFrame:
-    """
-    Compare a linear and mixed effects regression model.
-    Returns a dataframe of AIC and log-likelihood values extracted from both models.
-    """
-    res = []
-    for idx, grp in df.groupby('trial'):
-        # Create the linear model in statsmodels and fit
-        md_l = smf.ols(linear_mod, data=grp)
-        fit_l = md_l.fit()
-        # Create the mixed effects model in Pymer4 and fit
-        md_m = Lmer(mixed_mod, data=grp)
-        fit_m = md_m.fit(factors={
-            "latency": ["0", "23", "45", "90", "180"],
-            "jitter": ["0.0", "0.5", "1.0", ],
-            "instrument": ['Keys', 'Drums']
-        }, summary=False, no_warnings=True)
-        # Extract summary statistics for both models
-        res.append({
-            'trial': idx,
-            'linear_aic': fit_l.aic,
-            'linear_ll': fit_l.llf,
-            'mixed_aic': md_m.AIC,
-            'mixed_ll': md_m.logLike
-        })
-    return pd.DataFrame(res, index='trial')
+    df['predicted_bpm'] = 60 / df['predicted_ioi']
+    df['live_bpm'] = 60 / df['live_prev_ioi']
+    df['elapsed'] = np.floor(df['live_prev_onset'])
+    return df
 
 
 def pc_live_ioi_delayed_ioi(raw_data, output_dir):
@@ -161,32 +116,28 @@ def pc_live_ioi_delayed_ioi(raw_data, output_dir):
             f1 = lambda c: delay_heard_onsets_by_latency(append_zoom_array(generate_df(c['midi_bpm']), c['zoom_array']))
             keys = f1(c1)
             drms = f1(c2)
-
             # For each performer, carry out the nearest neighbor algorithm
             f2 = lambda la, da, li, di: format_df_for_model(nearest_neighbor(la, da, li, di))
             keys_nn = f2(keys['onset'].to_numpy(), drms['onset_delayed'].to_numpy(), c1['instrument'], c2['instrument'])
             drms_nn = f2(drms['onset'].to_numpy(), keys['onset_delayed'].to_numpy(), c2['instrument'], c1['instrument'])
-
             # For each performer, create the phase correction model
-            keys_md1 = construct_phase_correction_model(keys_nn, mod='live_next_ioi~live_prev_ioi+live_delayed_onset')
-            drms_md1 = construct_phase_correction_model(drms_nn, mod='live_next_ioi~live_prev_ioi+live_delayed_onset')
-            keys_md2 = construct_phase_correction_model(keys_nn, mod='live_next_ioi~live_prev_ioi+live_delayed_ioi')
-            drms_md2 = construct_phase_correction_model(drms_nn, mod='live_next_ioi~live_prev_ioi+live_delayed_ioi')
-
+            keys_md = construct_phase_correction_model(keys_nn, mod='live_next_ioi~live_prev_ioi+live_delayed_onset')
+            drms_md = construct_phase_correction_model(drms_nn, mod='live_next_ioi~live_prev_ioi+live_delayed_onset')
+            make_single_condition_phase_correction_plot(keys_df=predict_from_model(keys_md, keys_nn), keys_md=keys_md,
+                                                        drms_df=predict_from_model(drms_md, drms_nn), drms_md=drms_md,
+                                                        meta=(c1['trial'], c1['block'], c1['latency'], c1['jitter']),
+                                                        output=output_dir, keys_o=keys, drms_o=drms)
+            # Append the results
             tempo_slope = reg_func(
                 average_bpms(generate_df(c1['midi_bpm']), generate_df(c2['midi_bpm'])), ycol='bpm_avg', xcol='elapsed'
             ).params.iloc[1:].values[0]
-
-            # Append the results
-            f3 = lambda c, m1, m2, d: (c['trial'], c['block'], c['latency'], c['jitter'], c['instrument'], tempo_slope,
-                                       *m1.params.iloc[1:].values, *m2.params.iloc[1:].values)
-            res.append(f3(c1, keys_md1, keys_md2, keys_nn))
-            res.append(f3(c2, drms_md1, drms_md2, drms_nn))
+            f3 = lambda c, m1: (c['trial'], c['block'], c['latency'], c['jitter'], c['instrument'], tempo_slope,
+                                *m1.params.iloc[1:].values,)
+            res.append(f3(c1, keys_md))
+            res.append(f3(c2, drms_md))
             nn.append((c1['trial'], c1['block'], c1['latency'], c1['jitter'], keys_nn, drms_nn, tempo_slope))
-    df = pd.DataFrame(res, columns=['trial', 'block', 'latency', 'jitter', 'instrument', 'tempo_slope',
-                                    'correction_self_onset', 'correction_partner_onset', 'correction_self_ioi',
-                                    'correction_partner_ioi'])
-    make_pairgrid(df=df, xvar='correction_partner_ioi', output=output_dir)
-    make_pairgrid(df=df, xvar='correction_partner_onset', output=output_dir)
-    make_polar(nn_list=nn, output_dir=output_dir)
 
+    df = pd.DataFrame(res, columns=['trial', 'block', 'latency', 'jitter', 'instrument', 'tempo_slope',
+                                    'correction_self_onset', 'correction_partner_onset'])
+    make_pairgrid(df=df, output=output_dir, xvar='correction_partner_onset')
+    make_polar(nn_list=nn, output_dir=output_dir)

src/analyse/prepare_data.py
@@ -168,24 +168,31 @@ def zip_same_conditions_together(raw_data: list) -> list[zip]:
     return all_trials
 
 
-def average_bpms(df1: pd.DataFrame, df2: pd.DataFrame, window_size: int = 4) -> pd.DataFrame:
+def average_bpms(df1: pd.DataFrame, df2: pd.DataFrame, window_size: int = 4, elap: str = 'elapsed', bpm: str = 'bpm') -> pd.DataFrame:
     """
     Returns a list of averaged BPMs from two performance.
     Data is grouped by every second in a performance.
     """
     # Merge dataframes from both performers together
-    bigdf = df1.merge(df2, how='inner', on='elapsed')
+    bigdf = df1.merge(df2, how='inner', on=elap)
     # Set function
     fn = lambda g: g.drop_duplicates().dropna().tolist()
     # Average tempo of beat onsets created by both musicians within one second
     with warnings.catch_warnings():     # Catch errors when we don't have any values for one particular second
         warnings.simplefilter("ignore", category=RuntimeWarning)
-        avg_tempo = [(idx, np.nanmean([*fn(grp['bpm_x']), *fn(grp['bpm_y'])])) for idx, grp in bigdf.groupby('elapsed')]
+        avg_tempo = [(idx, np.nanmean([*fn(grp[f'{bpm}_x']), *fn(grp[f'{bpm}_y'])])) for idx, grp in bigdf.groupby(elap)]
     # Create dataframe
-    processed_df = pd.DataFrame(avg_tempo, columns=['elapsed', 'bpm_avg'])
+    df = pd.DataFrame(avg_tempo, columns=['elapsed', 'bpm_avg'])
+    # Reindex in case any rows are missing!
+    df.elapsed = df.elapsed.astype(int)
+    new_index = pd.Index(
+        [num for num in range(df.elapsed.min(), df.elapsed.max()+1)],
+        name="elapsed"
+    )
+    df = df.set_index('elapsed').reindex(new_index).reset_index()
     # Roll average BPM column
-    processed_df['bpm_rolling'] = processed_df['bpm_avg'].rolling(window=window_size).mean()
-    return processed_df
+    df['bpm_rolling'] = df['bpm_avg'].rolling(window=window_size, min_periods=1).mean()
+    return df
 
 
 def ioi_nearest_neighbours_intersection(a1, a2) -> list:

src/visualise/phase_correction_graphs.py
@@ -3,11 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.lines import Line2D
 import seaborn as sns
-import statsmodels.formula.api as smf
+
 import src.visualise.visualise_utils as vutils
+from src.analyse.prepare_data import average_bpms
 
 
-def make_pairgrid(df, output: str, xvar: str = 'correction_partner'):
+def make_pairgrid(df, output: str, xvar: str = 'correction_partner_onset'):
     """
     Creates a figure showing pairs of coefficients obtained for each performer in a condition,
     stratified by block and trial number, with shading according to tempo slope
@@ -63,19 +64,6 @@ def _format_pairgrid_fig(g, norm):
     g.fig.get_axes()[0].legend(loc='lower center', ncol=2, bbox_to_anchor=(2.85, -1.36), title=None, frameon=False)
     # Adjust the plot spacing
     g.fig.subplots_adjust(bottom=0.10, top=0.94, wspace=0.15, left=0.1, right=0.93)
-
-
-def create_prediction_plots(pred_list: list[tuple], output_dir: str):
-    test_li = [item for item in pred_list if item[0] == 4 and item[1] == 1 and item[2] == 45 and item[3] == 0]
-    km = test_li[0][4]
-    md = smf.ols('live_next_ioi~live_prev_ioi+live_delayed_onset+live_delayed_ioi', data=km).fit()
-    f = lambda d: (60 / d.dropna()).rolling(8).mean()
-    plt.plot(f(km['live_prev_ioi']), label='Data')
-    plt.plot(f(pd.Series(
-        np.insert(np.cumsum(md.predict()) + km['live_prev_onset'].loc[0], 0, km['live_prev_onset'].loc[0])).diff()),
-             label='Predicted')
-    plt.legend()
-    plt.show()
 
 
 def make_polar(nn_list: list[tuple], output_dir: str,) -> None:
@@ -178,13 +166,15 @@ def _format_polar_data(df: pd.DataFrame, num_bins: int = 10) -> pd.DataFrame:
     return cut
 
 
-def _format_polar_ax(ax: plt.Axes, sl) -> None:
+def _format_polar_ax(ax: plt.Axes, sl, yt: list = None, xt: list = None) -> None:
     """
     Formats a polar subplot by setting axis ticks and gridlines, rotation properties, and facecolor
     """
+    # Used to test if we've provided xticks already
+    test_none = lambda t: [] if t is None else t
     # Set the y axis ticks and gridlines
-    ax.set_yticks([])
-    ax.set_xticks([])
+    ax.set_yticks(test_none(yt))
+    ax.set_xticks(test_none(xt))
     ax.grid(False)
     # Set the polar plot rotation properties, direction, etc
     ax.set_thetamin(-90)
@@ -212,3 +202,83 @@ def _format_polar_fig(fig: plt.Figure, hand, lab, norm):
     # Create and position legend
     plt.legend(hand, lab, ncol=2, loc='lower center', bbox_to_anchor=(-46, -0.34), title=None,
                frameon=False, fontsize='xx-large')
+
+
+def make_single_condition_phase_correction_plot(keys_df: pd.DataFrame, drms_df: pd.DataFrame,
+                                                keys_md, drms_md,
+                                                keys_o: pd.DataFrame, drms_o: pd.DataFrame,
+                                                output: str,
+                                                meta: tuple = ('nan', 'nan', 'nan', 'nan'),):
+    """
+    Generate a nice plot of a single performance, showing actual and predicted tempo slope, relative phase adjustments,
+    phase correction coefficients to partner and self for both performers
+    """
+    out = vutils.create_output_folder(output, parent='phase_correction_models', child=f'individual\\duo_{meta[0]}')
+    # Create the matplotlib objects - figure with gridspec, 5 subplots
+    fig = plt.figure(figsize=(8, 8))
+    ax = vutils.get_gridspec_array(fig=fig)
+    # Plot onto the different parts of the gridspec
+    _single_fig_slopes(ax, keys_df=keys_df, drms_df=drms_df, keys_o=keys_o, drms_o=drms_o)
+    _single_fig_polar(ax, drms_df, keys_df)
+    _single_fig_coefficients(ax, drms_md, keys_md)
+    # Format the figure and save
+    fig.suptitle(f'Duo {meta[0]} (measure {meta[1]}): latency {meta[2]}ms, jitter {meta[3]}x')
+    fig.savefig(f'{out}\\duo{meta[0]}_measure{meta[1]}_latency{meta[2]}_jitter{meta[3]}.png')
+    plt.close()
+
+
+def _single_fig_polar(ax: plt.Axes, drms_df: pd.DataFrame, keys_df: pd.DataFrame) -> None:
+    """
+    For a plot of a single performance, create two polar plots showing phase difference between performers
+    """
+    # Create the polar plots
+    ax[0, 1].sharex(ax[0, 0])
+    for num, (ins, st) in enumerate(zip([keys_df, drms_df], ['Keys', 'Drums'])):
+        dat = _format_polar_data(ins, num_bins=15)
+        ax[0, num].bar(x=dat.idx, height=dat.val, width=0.05, label=st,
+                       color=vutils.data_cmap_contrast[0] if st == 'Keys' else vutils.data_cmap_contrast[1])
+        _format_polar_ax(ax=ax[0, num], sl='#ffffff', xt=np.pi / 180 * np.linspace(-90, 90, 5, endpoint=True),
+                         yt=[0, max(ax[0, num].get_yticks())])
+        ax[0, num].set(ylabel='', xlabel='')
+        x0, y0, x1, y1 = ax[0, num].get_position().get_points().flatten().tolist()
+        ax[0, num].set_position([x0-0.1 if num == 0 else x0-0.2, y0-0.03, x1-0.05 if num == 0 else x1-0.2, y1-0.5])
+        ax[0, num].text(-0.1, 0.8, s=st, transform=ax[0, num].transAxes, ha='center',)
+        if num == 0:
+            ax[0, num].text(1.5, 0.05, s="Relative Phase (πms)",
+                            transform=ax[0, num].transAxes, ha='center',)
+            ax[0, num].set_title('Relative Phase to Partner', x=1.5)
+
+
+def _single_fig_coefficients(ax: plt. Axes, drms_md, keys_md) -> None:
+    corr = pd.concat([keys_md.params, drms_md.params], axis=1).rename(
+        columns={0: 'Keys', 1: 'Drums'}).transpose().rename(
+        columns={'live_prev_ioi': 'Self', 'live_delayed_onset': 'Partner'}).reset_index(drop=False)
+    ax[1, 1].sharex(ax[1, 0])
+    for num, st in zip(range(0, 2), ['Keys', 'Drums']):
+        drms = corr[corr['index'] == st].melt().loc[2:]
+        g = sns.stripplot(ax=ax[1, num], x='value', y='variable', data=drms, jitter=False, dodge=False, marker='D', s=7,
+                          color=vutils.data_cmap_contrast[0] if st == 'Keys' else vutils.data_cmap_contrast[1])
+        g.set_xlim((-1.5, 1.5))
+        g.yaxis.grid(True)
+        g.xaxis.grid(False)
+        g.axvline(alpha=vutils.ALPHA, linestyle='-', color='#000000')
+        sns.despine(ax=g, left=True, bottom=True)
+        g.text(0.05, 0.9, s=st, transform=g.transAxes, ha='center', )
+        g.set_ylabel('')
+        if num == 0:
+            g.axes.set_yticks(g.axes.get_yticks(), g.axes.get_yticklabels())
+            g.axes.set_yticklabels(labels=g.axes.get_yticklabels(), va='center')
+            g.set_title('Phase Correction', x=1)
+            g.set_xlabel('Coefficient', x=1)
+        else:
+            g.axes.set_yticklabels('')
+            g.set_xlabel('')
+
+
+def _single_fig_slopes(ax: plt.Axes, keys_df: pd.DataFrame, drms_df: pd.DataFrame, keys_o: pd.DataFrame, drms_o: pd.DataFrame):
+    # Plot the actual and predicted rolling tempo slope
+    func = lambda s: average_bpms(keys_df, drms_df, elap='elapsed', bpm=s)
+    for df, lab in zip((average_bpms(keys_o, drms_o), func('predicted_bpm')), ('Actual', 'Fitted')):
+        ax[2, 0].plot(df['elapsed'], df['bpm_rolling'], label=lab)
+    ax[2, 0].legend()
+    ax[2, 0].set(xlabel='Performance Duration (s)', ylabel='Average tempo (BPM)', ylim=(30, 160), title='Tempo Slope')

src/visualise/visualise_utils.py
@@ -1,15 +1,18 @@
 from pathlib import Path
+from matplotlib import pyplot as plt
 from matplotlib.colors import TwoSlopeNorm, ListedColormap
 from matplotlib.cm import ScalarMappable
 import numpy as np
+import pandas as pd
 from seaborn import color_palette
 from statistics import median
 
 # Define constants
 ALPHA = 0.4
 OFFSET = 8
+VIDEO_FPS = 30
 
-# Function used to shade a cmap by given alpha value (can be used in colobars etc)
+# Function used to shade a cmap by given alpha value (can be used in colorbars etc)
 cmap_alpha = lambda pal: ListedColormap(np.c_[pal.colors, np.full(len(pal.colors), fill_value=ALPHA)])
 
 # Define the colour palettes
@@ -45,3 +48,45 @@ def create_scalar_cbar(norm: TwoSlopeNorm) -> ScalarMappable:
     Creates a scalar colourbar object to be placed on a figure
     """
     return ScalarMappable(norm=norm, cmap=slopes_cmap)
+
+
+def get_gridspec_array(fig: plt.Figure = None, ncols: int = 2) -> np.array:
+    """
+    Create an array of axes with unequal numbers of plots per row/column
+    Returns an array that can be indexed in the same way as the array normally returned by plt.subplots()
+    """
+    if fig is None:
+        fig = plt.figure()
+    # Create the gridspec
+    gs = fig.add_gridspec(3, ncols, height_ratios=[1, 2, 3],
+                          wspace=0.1, hspace=0.3, top=0.92, bottom=0.07)
+    # Create an array of axes with uneven numbers of plots per row
+    return np.array(
+        [[*(fig.add_subplot(gs[1, num], projection='polar') for num in range(0, ncols))],   # For the phase diff polar
+         [*(fig.add_subplot(gs[0, num]) for num in range(0, ncols))],   # For the phase correction coefficients
+         [fig.add_subplot(gs[2, :]), np.nan]]  # For the tempo slope
+    )
+
+
+def append_count_in_rows_to_df(df_avg_slopes: pd.DataFrame, time_col: str = 'elapsed') -> pd.DataFrame:
+    """
+    Appends empty rows to a dataframe corresponding with every second of the count-in where no notes were played.
+    Input dataframe should be in the format returned by average_bpms (i.e. average BPM across performers per second)
+    Used when creating videos of tempo slopes.
+    """
+    # Create a new dataframe of the extra rows we need to go from 0 to performance start
+    add = pd.DataFrame([(num, np.nan, np.nan) for num in range(0, int(df_avg_slopes.loc[0][time_col]))],
+                       columns=df_avg_slopes.columns)
+    # Concat the new dataframe and our input
+    return pd.concat([add, df_avg_slopes]).reset_index(drop=True)
+
+
+def interpolate_df_rows(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Adds additional rows to a tempo slope dataframe, used when upscaling video FPS.
+    """
+    # Create the new index
+    new_indx = np.linspace(df.index.min(), df.index.max(),
+                           (df.shape[0] - 1) * (VIDEO_FPS - 1) + df.shape[0])
+    # Reindex the input dataframe and interpolate according to it
+    return df.reindex(new_indx).interpolate(method='index').reset_index(drop=False)