02b4afa3cb5233a135079f1c388d8367c1617c98
Huw Cheston <hwc31@cam.ac.uk>
2023-04-12 14:58:43 +0100

    Reverted changes to run.cmd

--- run.cmd
+++ run.cmd
@@ -6,12 +6,12 @@ python -m venv venv
 call venv\Scripts\activate.bat
 python -m pip install wheel
 echo Testing virtual environment...
-python -m test_environment.py
+python test_environment.py
 echo Installing project requirements from requirements.txt...
 python -m pip install -r requirements.txt
 echo Setup completed successfully!
-python -m src\clean\make_dataset.py -i "data\raw" -o "data\processed"
-python -m src\analyse\run_analysis.py -i "data\processed" -o "models"
+python src\clean\make_dataset.py -i "data\raw" -o "data\processed"
+python src\analyse\run_analysis.py -i "data\processed" -o "models"
 echo Cleaning up...
 deactivate
 echo Done!
